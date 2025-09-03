c9ee7a627caa75d4d6bfb196c9f4b5157e10df1f
quang0510 <nguyendangquang0510@gmail.com>
2025-09-03 20:31:40 +0700

    Update Bai8.tsx

--- ss21/src/components/Bai8.tsx
+++ ss21/src/components/Bai8.tsx
@@ -1,36 +1,37 @@
 export default function Bai8() {
   return (
     <div className="flex gap-10 flex-col">
-      <div className="border left flex gap-4">
+      <div className="border flex gap-4">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
-      <div className="border left flex gap-4 justify-end">
+      <div className="border flex gap-4 justify-end">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
-      <div className="border left flex gap-4 justify-center">
+      <div className="border flex gap-4 justify-center">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
-      <div className="border left flex gap-4 justify-between">
+      <div className="border flex gap-4 justify-between">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
-      <div className="border left flex gap-4 justify-around">
+      <div className="border flex gap-4 justify-around">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
-      <div className="border left flex gap-4 justify-evenly">
+      <div className="border flex gap-4 justify-evenly">
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">01</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">02</div>
         <div className="box bg-[#3C82F6] p-5 text-white rounded-md">03</div>
       </div>
     </div>
   );
+
 }
